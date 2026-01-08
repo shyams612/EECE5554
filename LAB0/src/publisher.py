d0ce21f8e68b4c1e6044f15804990ce0da075651
Shyam Sreenivasan <shyamsreeni.612@gmail.com>
2026-01-07 19:53:14 -0500

    first commit - added psimple publisher subscriber

---
+++ LAB0/src/publisher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SimplePublisher(Node):
+    def __init__(self):
+        super().__init__('simple_publisher')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.counter = 0
+        self.get_logger().info('Publisher has been started')
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Hello ROS2! Message count: {self.counter}'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+        self.counter += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SimplePublisher()
+    
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
